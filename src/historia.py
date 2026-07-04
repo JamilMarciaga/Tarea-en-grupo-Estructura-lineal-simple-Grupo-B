be9833b24ee1caf40c3daaa72a87b0e764481bf8
JamilMarciaga <jamil.marciaga@uip.edu.pa>
2026-07-03 21:58:34 -0500

    Agrego mi Parte 1 - Jamil

---
+++ src/historia.py
@@ -0,0 +1,11 @@
+# ============================================
+# TAREA 1: Historia Interactiva
+# Estructura Lineal Simple - Grupo B
+# ============================================
+
+# PARTE 1 - CREADA POR: Jamil
+nombre = input("Ingresa tu nombre: ")
+edad = input("Ingresa tu edad: ")
+ciudad = input("Ingresa tu ciudad de residencia: ")
+
+print("¡Hola, " + nombre + "! Tienes " + edad + " años y vives en " + ciudad + ".")
