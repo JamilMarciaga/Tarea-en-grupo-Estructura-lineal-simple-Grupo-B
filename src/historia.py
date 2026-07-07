5f4164bc0428f6217e1a044ee594b1203baf0ad1
Alexis Rafael Lopez Arconategui <alexis.lopez@sofka.com.co>
2026-07-06 22:15:19 -0500

    feat: agrega parte 3 de metas personales por Alexis

--- src/historia.py
+++ src/historia.py
@@ -14,6 +14,11 @@ print("¡Hola, " + nombre + "! Tienes " + edad + " años y vives en " + ciudad +
 estudio = input("¿Qué estudias o en qué trabajas?: ")
 print(nombre + " estudia o trabaja en " + estudio + ".")
 
+# PARTE 3 - CREADA POR: Alexis
+meta_personal = input("¿Cuál es tu meta personal principal?: ")
+motivacion_meta = input("¿Qué te motiva a alcanzar esa meta?: ")
+print(nombre + " tiene como meta personal " + meta_personal + ". Su motivación para alcanzar esta meta es " + motivacion_meta + ".")
+
 # PARTE 4 - CREADA POR: Esequiel
 final = input("¿Qué ocurrió al final de la historia?: ")
 print("Finalmente, " + nombre + " " + final + ".")
